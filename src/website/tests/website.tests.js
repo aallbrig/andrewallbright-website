72b00847e793adb4d0704f97ba9050805e722269
Andrew Allbright <aallbrig@gmail.com>
2023-11-05 07:05:55 -0500

    Add new 'can load' assertions for current website pages of concern

--- src/website/tests/website.tests.js
+++ src/website/tests/website.tests.js
@@ -1,24 +1,48 @@
 const puppeteer = require('puppeteer');
 const { expect } = require('chai');
 
-const website_host = process.env.WEBSITE_HOST; // protocol + hostname + port
+// root of website in scheme + domain name + port format e.g. https://example.com
+const website_url = new URL(process.env.WEBSITE_URL);
+const blog_archive_url = new URL('/blog/', website_url);
+const contact_url = new URL('/contact/', website_url);
+
+const puppeteer_setting_headless = process.env.PUPPETEER_SETTING_HEADLESS !== 'false';
+const puppeteer_options = {
+    headless: puppeteer_setting_headless
+};
 
 describe('Personal Website Behavior', function() {
+    let browser;
+    let page;
+
+    before(async function() {
+        browser = await puppeteer.launch(puppeteer_options);
+    });
+    beforeEach(async function() {
+        page = await browser.newPage();
+    });
+    afterEach(async function() {
+        await page.close();
+    });
+    after(async function() {
+        await browser.close();
+    });
+
     describe('Home Page', function() {
-        let page;
-        let browser;
-
-        before(async function() {
-            browser = await puppeteer.launch();
-            page = await browser.newPage();
-        });
-
-        after(async function() {
-            await browser.close();
-        });
-
         it('Should load', async function() {
-            const response = await page.goto(website_host);
+            const response = await page.goto(website_url);
+            expect(response.status()).to.equal(200);
+        });
+    });
+    describe('Blog Archive', function() {
+        it('Should load', async function() {
+            const response = await page.goto(blog_archive_url);
+            expect(response.status()).to.equal(200);
+        });
+    });
+    describe('Contact Form', function() {
+        it('Should load', async function() {
+            const response = await page.goto(contact_url);
             expect(response.status()).to.equal(200);
         });
     });
